9bc2450c678d98f80dc021962f4f91e34334f90d
MAŁGORZATA FEDOROWICZ <malgorzata.fedorowicz81@gmail.com>
2023-03-09 22:09:36 +0100

    zadanie dzień 4

---
+++ ChallengeApp2/ChallengeApp2/Program.cs
@@ -0,0 +1,18 @@
+﻿string name = "Ewa";
+var Age = 18;
+char myVar = 'k';
+
+if (name == "Ewa" && Age == 18)
+{
+    Console.WriteLine("kobieta poniżej 30 lat");
+}
+else if (Age < 18 && myVar == 'm')
+{
+    Console.WriteLine("niepełnoletni mężczyzna");
+}
+else if (name == "Ewa" && Age == 33)
+{
+    Console.WriteLine("kobieta lat 33");
+}
+
+
